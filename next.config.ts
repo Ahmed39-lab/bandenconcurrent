f3d8742fbe1bfb32e51add19e18c70975317855f
Ahmed39-lab <AhmedJanWorks@gmail.com>
2026-01-27 18:43:24 +0500

    protocol added

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
   images: {
-    domains: ["images.example.com", "hostbrake.com","localhost","placehold.co",
-    "bandenconcurrent-production.up.railway.app"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bandenconcurrent-production.up.railway.app",
+      },
+      {
+        protocol: "https",
+        hostname: "hostbrake.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
   },
 };
 
