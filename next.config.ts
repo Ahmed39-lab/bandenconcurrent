4844ec2bfa2e04f2be84ae62865d71ca3aea4559
Ahmed39-lab <AhmedJanWorks@gmail.com>
2026-02-13 21:19:29 +0500

    domain allowed

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["images.example.com", "hostbrake.com","localhost","placehold.co",
-    "bandenconcurrent-production.up.railway.app","res.cloudinary.com"],
+    domains: [
+      "images.example.com",
+      "hostbrake.com",
+      "localhost",
+      "placehold.co",
+      "bandenconcurrent-production.up.railway.app",
+      "res.cloudinary.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.ngrok-free.dev",
+      },
+    ],
   },
 };
 
