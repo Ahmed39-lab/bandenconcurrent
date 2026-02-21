71ec00bfb137317ff113c38c800de5dbedbd5bb2
Ahmed39-lab <AhmedJanWorks@gmail.com>
2026-02-21 17:50:43 +0500

    config files added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: [
       "images.example.com",
@@ -17,6 +16,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    allowedDevOrigins: ['https://snelbanden.nl'],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
